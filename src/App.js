15f8b597ca7c0e9bb50acdeb1bdba8987e823e9f
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-04-25 00:57:16 -0300

    Learning react context and hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import NewBookForm from './components/BookForm';
+import BookList from './components/BookList';
+import NavBar from './components/NavBar';
+import BookContextProvider from './context/BookContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookContextProvider>
+        <NavBar />
+        <BookList />
+        <NewBookForm />
+      </BookContextProvider>
     </div>
   );
 }
